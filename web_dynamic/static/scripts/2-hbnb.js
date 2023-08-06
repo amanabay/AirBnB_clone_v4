0c5efc66fc1146d392d1190a886055898bfed15c
amanabay <amanuelabay22@gmail.com>
2023-08-06 15:39:02 +0300

    Show api status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  // Store the checked Amenity IDs
+  const checkedAmenities = {};
+
+  $('.amenity-checkbox').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      delete checkedAmenities[amenityId];
+    }
+    $('.amenities h4').text(Object.values(checkedAmenities).join(', '));
+  });
+
+  $.get('http://127.0.0.1:5001/api/v1/status', function (data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
